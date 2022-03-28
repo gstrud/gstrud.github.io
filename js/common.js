d200261d055e7740a02c38497e6fc8fd232b352d
gstrud <gstrud@mail.ru>
2022-03-28 14:28:19 +0300

    Admin
    js++

--- js/common.js
+++ js/common.js
@@ -27,13 +27,22 @@ check.onclick = function(){FunChbox();};
 
 
 
-//------------------------------------
-/*
-//TEST
-function handler() {
-  alert( 'Спасибо!' );
-};
-
-//test = document.getElementById('test');
-test.addEventListener("click", handler);
-*/
+//Создать блок <DIV>  с классом 'content' в HTML-документе
+document.querySelector('.content').innerHTML = `<div class="job-list"></div>`;
+try {
+  for (key in companyObjectList) {
+    let company = document.createElement('h2');
+    company.innerHTML = `${key}`;
+    document.querySelector('.job-list').appendChild(company);
+    let ul = document.createElement('ul');
+    for (let index = 0; index < companyObjectList[key].length; index++) {
+      ul.innerHTML += `<li><span>${companyObjectList[key][index][0]}</span><p>${companyObjectList[key][index][1]}</p></li>`;
+      document.querySelector('.job-list').appendChild(ul);
+    }
+  }
+} catch (err) {
+  //alert('Ошибка ' + err.name + ':' + err.message + '\n' + err.stack);
+  alert('Извините, в данных ошибка');
+  alert(err.name);
+  alert(err.message);
+}
